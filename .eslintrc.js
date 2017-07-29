005f7e6b4a22c4013559f57807e6645f12854dc2
Adam Burke <adam@Adams-Mac-mini.local>
2017-07-29 20:40:30 +1000

    first-commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['node'],
+  env: {
+    node: true
+  },
+  rules: {
+    'react/wrap-multilines': 0,
+    'react/require-extension': 0
+  }
+};
